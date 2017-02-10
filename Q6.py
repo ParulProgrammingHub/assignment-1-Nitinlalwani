0acb5826c8a4341a37b648d0c345fac6819fcbc1
Nitin lalwani <nitindlalwani@gmail.com>
2017-02-10 19:38:20 +0530

    Q6

---
+++ Q6.py
@@ -0,0 +1,6 @@
+#program to finding power of any number in form  x^y where x and y are user input.
+
+no =int ( input ("enter the no.:"))
+power = int (input("enter the power :"))
+ans = no ** power
+print ("answer is:",ans)
